b0b4faa734dbdc723887bd2af425ad4d753a55b9
Jonathan Woline <jonwoline@gmail.com>
2025-04-27 05:24:59 -0700

    Updated ViewItem page

--- front/src/components/ViewItem.jsx
+++ front/src/components/ViewItem.jsx
@@ -34,6 +34,7 @@ function ViewClientPage() {
     const [item, setItem] = useState({});
     const [description, setDescription] = useState("");
     const [title, setTitle] = useState("");
+    const [status, setStatus] = useState(1);
     const [loading, setLoading] = useState(true);
 
     const update_item = async () => {
@@ -48,6 +49,7 @@ function ViewClientPage() {
                 setItem(found_item);
                 setDescription(found_item.item_description);
                 setTitle(found_item.item_name);
+                setStatus(found_item.status);
                 return;
             }
         }
@@ -60,7 +62,9 @@ function ViewClientPage() {
         }
     }, [])
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('a');
         return;
     }
 
@@ -69,16 +73,22 @@ function ViewClientPage() {
         <div className="flex flex-col overflow-y-auto justify-between h-max">
         {
             loading ? <Loading></Loading> :
-            <>
-            <form className="flex flex-col space-y-3" onSubmit={handleSubmit}>
-                <div className="text-2xl">Change Status</div>
-                <button onClick={() => {console.log('a')}} className={`text-lg cursor-pointer p-3 ${status_to_style(item.status)} text-white w-full text-center`}>{ status_to_text(item.status) }</button>
-                <div className="text-2xl">Edit Name</div>
-                <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={title} onChange={(element) => {setTitle(element.value)}}></input>
-                <div className="text-2xl">Edit Description</div>
-                <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={description} onChange={(element) => {setDescription(element.value)}}></input>
-            </form>
-            </>
+            <div className="flex sm:flex-row flex-col justify-between sm:overflow-y-visible items-center min-h-fit overflow-y-scroll">
+                <form className="flex flex-col space-y-3 w-full p-4" onSubmit={handleSubmit}>
+                    <div className="text-2xl">Change Status</div>
+                    <button onClick={() => {console.log('a')}} className={`text-lg text-left cursor-pointer p-3 ${status_to_style(item.status)} text-white w-full text-center`}>{ status_to_text(item.status) }</button>
+                    <div className="text-2xl">Edit Name</div>
+                    <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={title} onChange={(element) => {setTitle(element.value)}}></input>
+                    <div className="text-2xl">Edit Description</div>
+                    <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={description} onChange={(element) => {setDescription(element.value)}}></input>
+                    <button className="bg-green-600 text-black hover:bg-healthygreen p-2 mt-6 cursor-pointer" type="submit" value="Submit">Submit</button>
+                </form>
+                <div className="w-fit mt-10 sm:h-fit h-[10px] mx-10">
+                    <div className="w-[256px] h-[256px] bg-black text-white justify-center align-middle">
+                        QR Code Here
+                    </div>
+                </div>
+            </div>
             }
         </div>
     </div>
