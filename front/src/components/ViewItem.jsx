2c808da70f469c95d02c8d1b8525ca9208ca42be
Jonathan Woline <jonwoline@gmail.com>
2025-04-27 04:48:15 -0700

    Updated ViewItem page

--- front/src/components/ViewItem.jsx
+++ front/src/components/ViewItem.jsx
@@ -32,34 +32,52 @@ function status_to_text(status)
 function ViewClientPage() {
     const { uuid } = useParams();
     const [item, setItem] = useState({});
+    const [description, setDescription] = useState("");
+    const [title, setTitle] = useState("");
     const [loading, setLoading] = useState(true);
 
     const update_item = async () => {
         const items = await get_items();
+        
+        setLoading(false);
 
         for (const found_item of items)
         {
             if (found_item.uuid == uuid)
             {
                 setItem(found_item);
+                setDescription(found_item.item_description);
+                setTitle(found_item.item_name);
                 return;
             }
         }
     }
 
     useEffect(() => {
-        update_item();
-        setLoading(false);
+        if (loading)
+        {
+            update_item();
+        }
     }, [])
 
-    return (<div className="h-screen ml-6 mr-6 p-1 w-full">
+    const handleSubmit = () => {
+        return;
+    }
+
+    return (<div className="h-full ml-6 mr-6 p-1 w-full">
     <div className="text-3xl mb-5 font-bold">{ item.item_name }</div>
-        <div className="overflow-y-auto h-full">
+        <div className="flex flex-col overflow-y-auto justify-between h-max">
         {
             loading ? <Loading></Loading> :
             <>
-            <div className="text-md w-full text-left p-2 bg-white overflow-clip text-ellipsis">{ item.item_description }</div>
-            <div className={`text-lg ${status_to_style(item.status)} text-white w-full text-center`}>{ status_to_text(item.status) }</div>
+            <form className="flex flex-col space-y-3" onSubmit={handleSubmit}>
+                <div className="text-2xl">Change Status</div>
+                <button onClick={() => {console.log('a')}} className={`text-lg cursor-pointer p-3 ${status_to_style(item.status)} text-white w-full text-center`}>{ status_to_text(item.status) }</button>
+                <div className="text-2xl">Edit Name</div>
+                <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={title} onChange={(element) => {setTitle(element.value)}}></input>
+                <div className="text-2xl">Edit Description</div>
+                <input className="border border-green-600 p-2 focus:outline-none focus:border-2" type="text" value={description} onChange={(element) => {setDescription(element.value)}}></input>
+            </form>
             </>
             }
         </div>
